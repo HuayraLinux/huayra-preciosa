e13af40289a5978306cc12ac7095053ea183dcab
Ignacio Benedetti <tranceway@gmail.com>
2014-04-21 12:14:20 -0300

    Cambios finales de ayuda

--- www/js/loader.js
+++ www/js/loader.js
@@ -9,3 +9,11 @@ $( document ).on( "mobileinit", function() {
     $.mobile.loader.prototype.options.theme = "b";
     $.mobile.loader.prototype.options.html = "";
 });
+
+var chilpro = require('child_process');
+
+$('.ayuda_visor').on('click', function(){
+    var comando = '/usr/bin/huayra-visor-manual "articles/p/r/e/Preciosa.html"';
+    chilpro.exec(comando);
+    return false;
+});
